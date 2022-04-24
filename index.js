79ef75e25f4663affb26cdad41866926ded6466e
johncalabro <j.calabro4@gmail.com>
2022-04-24 13:25:49 -0400

    commit tree fb32c9a12c35259db65f2292e6704b08bb0f78c9

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your code here//
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+multiply;
+
+let random = Math.floor(Math.random() * 10)
+
+let num3 = 18
+let num4 = 7
+let mod = num3 % num4
+
+let max = Math.max (1, 3, 20)
